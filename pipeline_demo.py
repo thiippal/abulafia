34f4b1cf415e1762206eb851461b76513308939d
thiippal <tuomo.hiippala@iki.fi>
2022-01-14 10:59:20 +0200

    update pipeline demo

--- pipeline_demo.py
+++ pipeline_demo.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from tasks import ImageClassificationTask, InputData
+from task_specs import ImageClassificationTask, ImageSegmentationTask
+from pipeline import TaskSequence
 import json
 import toloka.client as toloka
-
 
 # Read the credentials
 with open('creds.json') as cred_f:
@@ -11,7 +11,14 @@ with open('creds.json') as cred_f:
     creds = json.loads(cred_f.read())
     tclient = toloka.TolokaClient(creds['token'], creds['mode'])
 
+exam = ImageClassificationTask(configuration='text_exam.yaml', client=tclient)
 
-input_data = InputData('data/exam.tsv')
-pipe = ImageClassificationTask(configuration='exam.json', client=tclient)(input_data)
+main = ImageClassificationTask(configuration='text_detect.yaml', client=tclient)
 
+outline = ImageSegmentationTask(configuration='text_outline.yaml', client=tclient)
+
+y = TaskSequence(sequence=[exam, main, outline], client=tclient)
+
+y.start()
+
+exit()
