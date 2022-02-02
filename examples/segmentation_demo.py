c186307d53d4f584e1bc4de939f0c56f6116bc70
thiippal <tuomo.hiippala@iki.fi>
2022-02-02 21:48:47 +0200

    examples

--- examples/segmentation_demo.py
+++ examples/segmentation_demo.py
@@ -3,21 +3,21 @@
 import sys
 sys.path.append('..')
 
-from task_specs import ImageClassification, TaskSequence
+from task_specs import ImageSegmentation, TaskSequence
 import json
 import toloka.client as toloka
 
 # Read the credentials from the JSON file
-with open('creds.json') as cred_f:
+with open('../creds.json') as cred_f:
 
     creds = json.loads(cred_f.read())
     tclient = toloka.TolokaClient(creds['token'], creds['mode'])
 
-# Create an ImageClassification task with training and exam
-exam = ImageClassification(configuration='tasks/text_exam.yaml', client=tclient)
+# Create an ImageSegmentation task using the configuration file
+outline = ImageSegmentation(configuration='tasks/text_outline.json', client=tclient)
 
 # Add the task into a TaskSequence
-sequence = TaskSequence(sequence=[exam], client=tclient)
+sequence = TaskSequence(sequence=[outline], client=tclient)
 
 # Start the sequence; create tasks on Toloka
 sequence.start()
