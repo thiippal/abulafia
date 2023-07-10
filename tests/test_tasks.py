89a4052a0a205b8728ee047b864708198e423d0a
thiippal <tuomo.hiippala@iki.fi>
2023-07-10 22:04:50 +0300

    Update tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -101,22 +101,51 @@ class TestProject:
     def test_project_task_spec(self, test_task):
 
         # Build a task interface similar to the one defined in data/detect_text.yaml
-        task_spec = toloka.project.task_spec.TaskSpec(input_spec={'image': toloka.project.UrlSpec(required=True,
-                                                                                                  hidden=False)},
-                                                      output_spec={'result': toloka.project.BooleanSpec(required=True,
-                                                                                                        hidden=False,
-                                                                                                        allowed_values=None)},
-                                                      view_spec=toloka.project.TemplateBuilderViewSpec(settings=None,
-                                                                                                       config=toloka.project.template_builder.TemplateBuilder(
-                                                                                                           view=toloka.project.template_builder.ListViewV1(items=[
-                                                                                                               toloka.project.template_builder.ImageViewV1(url=toloka.project.template_builder.InputData(path='image', default=None), full_height=True, max_width=None, min_width=None, no_border=None, no_lazy_load=None, popup=None, ratio=None, rotatable=True, scrollable=None, hint=None, label=None, validation=None, version='1.0.0'),
-                                                                                                               toloka.project.template_builder.TextViewV1(content='Does the diagram contain text, letters or numbers?', hint=None, label=None, validation=None, version='1.0.0'),
-                                                                                                               toloka.project.template_builder.ButtonRadioGroupFieldV1(data=toloka.project.template_builder.OutputData(path='result', default=None),
-                                                                                                                                                                       options=[toloka.project.template_builder.GroupFieldOption(value=True, label='Yes', hint=None),
-                                                                                                                                                                                toloka.project.template_builder.GroupFieldOption(value=False, label='No', hint=None)], hint=None, label=None, validation=toloka.project.template_builder.RequiredConditionV1(data=None, hint='You must choose one response.', version='1.0.0'), version='1.0.0')], direction=None, size=None, hint=None, label=None, validation=None, version='1.0.0'),
-                                                                                                           plugins=[toloka.project.template_builder.TolokaPluginV1(layout=toloka.project.template_builder.TolokaPluginV1.TolokaPluginLayout(kind='scroll', task_width=500), notifications=None, version='1.0.0'),
-                                                                                                                    toloka.project.template_builder.HotkeysPluginV1(key_1=toloka.project.template_builder.SetActionV1(data=toloka.project.template_builder.OutputData(path='result', default=None), payload=True, version='1.0.0'),
-                                                                                                                                                                    key_2=toloka.project.template_builder.SetActionV1(data=toloka.project.template_builder.OutputData(path='result', default=None), payload=False, version='1.0.0'), version='1.0.0')], vars=None), core_version='1.0.0', infer_data_spec=False))
+        task_spec = toloka.project.task_spec.TaskSpec(
+            input_spec={'image': toloka.project.UrlSpec(required=True, hidden=False),
+                        'assignment_id': toloka.project.StringSpec(required=False, hidden=False)},
+            output_spec={'result': toloka.project.BooleanSpec(required=True, hidden=False, allowed_values=None)},
+            view_spec=toloka.project.TemplateBuilderViewSpec(
+                settings=None,
+                config=toloka.project.template_builder.TemplateBuilder(
+                    view=toloka.project.template_builder.ListViewV1(
+                        items=[toloka.project.template_builder.ImageViewV1(
+                            url=toloka.project.template_builder.InputData(
+                                path='image', default=None),
+                            full_height=True,
+                            max_width=None,
+                            min_width=None,
+                            no_border=None,
+                            no_lazy_load=None,
+                            popup=None,
+                            ratio=None,
+                            rotatable=True,
+                            scrollable=None,
+                            hint=None,
+                            label=None,
+                            validation=None,
+                            version='1.0.0'),
+                            toloka.project.template_builder.TextViewV1(
+                                content='Does the diagram contain text, letters or numbers?',
+                                hint=None, label=None, validation=None, version='1.0.0'),
+                            toloka.project.template_builder.ButtonRadioGroupFieldV1(
+                                data=toloka.project.template_builder.OutputData(path='result', default=None),
+                                options=[toloka.project.template_builder.GroupFieldOption(value=True, label='Yes', hint=None),
+                                         toloka.project.template_builder.GroupFieldOption(value=False, label='No', hint=None)],
+                                hint=None, label=None, validation=toloka.project.template_builder.RequiredConditionV1(
+                                    data=None, hint='You must choose one response.', version='1.0.0'), version='1.0.0')],
+                        direction=None, size=None, hint=None, label=None, validation=None, version='1.0.0'),
+                    plugins=[toloka.project.template_builder.TolokaPluginV1(
+                        layout=toloka.project.template_builder.TolokaPluginV1.TolokaPluginLayout(
+                            kind='scroll', task_width=500), notifications=None, version='1.0.0'),
+                        toloka.project.template_builder.HotkeysPluginV1(
+                            key_1=toloka.project.template_builder.SetActionV1(
+                                data=toloka.project.template_builder.OutputData(path='result', default=None),
+                                payload=True, version='1.0.0'),
+                            key_2=toloka.project.template_builder.SetActionV1(
+                                data=toloka.project.template_builder.OutputData(path='result', default=None),
+                                payload=False, version='1.0.0'), version='1.0.0')], vars=None),
+                core_version='1.0.0', infer_data_spec=False))
 
         assert test_task.project.task_spec == task_spec
 
